33c0e596f420c2a6310a3d7bd1284cf1652cab5b
EliverLara <eliverlara@gmail.com>
2017-10-12 20:07:29 -0500

    Add shell theme

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,7 +9,18 @@ gulp.task('styles', function() {
         .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "Ant"'))
 });
 
+gulp.task('shell-style', function() {
+    gulp.src('gnome-shell/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./gnome-shell/'))
+        .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "Ant"'))
+});
+
 //Watch task
 gulp.task('default',function() {
     gulp.watch('gtk-3.20/**/*.scss',['styles']);
+});
+
+gulp.task('shell',function() {
+    gulp.watch('gnome-shell/*.scss',['shell-style']);
 });
